eee26053fc46f95eb982bbfdf301feeea2bc5c6f
Deepanshu Saini <sainids785@gmail.com>
2022-11-20 15:18:41 +0530

    a desktop app with github API

---
+++ github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '671ed02c4b22a281e44f';
+const githubClientSecret = '604fdde32fb1a655115239dcf237a6788dae0557';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
